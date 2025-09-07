30a689050ea7f89c9b69b3ca14a4943d41bc3f49
Decoder4code <115099652+Decoder4code@users.noreply.github.com>
2025-09-07 17:17:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let count=0;
+function inc(){
+    count++;
+    document.getElementById("count").innerText=count;
+}
+function reset(){
+    count=0;
+    document.getElementById("count").innerText=0;
+}
